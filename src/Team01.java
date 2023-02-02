d87de90faeb0f5aa08b4d35358efc9c1d530c57f
Nathan <garayn@moravian.edu>
2023-02-02 10:02:02 -0500

    Message

---
+++ src/Team01.java
@@ -0,0 +1,19 @@
+package Project00;
+
+import java.util.*;
+import Project00.Team;
+import Project00.Member;
+import Project00.TeamMember;
+
+public class Team01 extends Team {
+    public Team01(String team01) {
+        super(team01);
+        createTeamMembers();
+    }
+
+    @Override
+    public void createTeamMembers()
+    {
+        members.add(new TeamMember("Nate Garay"));
+    }
+}
